bb13cc712d6f37251990556b432f6c570c80c586
DimasCW <cdimas292@gmail.com>
2023-05-19 11:11:34 +0700

    membuat perulangan dalam method addnote

--- 163_singlelinklist/163_singlelinklist.cpp
+++ 163_singlelinklist/163_singlelinklist.cpp
@@ -3,9 +3,49 @@ using namespace std;
 
 struct node {
 	int nomhs;
-	string nama;
+	string name;
 	node* next;
 };
 
 node* start = NULL;
+
+void addnote(){
+	int nim;
+	string nama;
+	node* nodebaru = new node();
+	cout << "masukkan nim : ";
+	cin >> nim;
+	cout << "masukkan nama = ";
+	cin >> nama;
+	nodebaru->nomhs = nim;
+	nodebaru->name = nama;
+
+	if (start == NULL || nim <= start->nomhs) {
+		if (start != NULL && nim == start->nomhs) {
+			cout << "nim sudah ada" << endl;
+			return;
+		}
+
+		node* current = start;
+		node* previous = start;
+
+		while ((current != NULL) && (nim >= current->nomhs)) {
+			if (nim == current->nomhs) {
+				cout << "nim sudah ada" << endl;
+				return;
+			}
+			previous = current;
+			current = current->next;
+		}
+	}
+
+}
+
+void{
+
+};
+
+void{
+
+};
 
