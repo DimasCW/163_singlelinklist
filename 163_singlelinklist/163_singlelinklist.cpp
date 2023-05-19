fe9220143d618c212aebb3832c5920f8c23dd150
DimasCW <cdimas292@gmail.com>
2023-05-19 18:38:45 +0700

    membuat case 1 dan 2

--- 163_singlelinklist/163_singlelinklist.cpp
+++ 163_singlelinklist/163_singlelinklist.cpp
@@ -133,4 +133,32 @@ int main() {
 			cout << "5. Keluar" << endl;
 			cout << "Pilihan : ";
 			cin >> pilihan;
+			switch (pilihan)
+			{
+			case 1:
+				addnote();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+				int nim;
+
+				cout << "Masukkan NIM : ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim : " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
 }
